ccabe89bb0443dff3be2a2517f3ae568c2f06726
jerlendds <jerlendds@proton.me>
2025-05-31 10:48:19 -0600

    add sql roles and permissions tables

--- rs/migrations/20250529170043_init.up.sql
+++ rs/migrations/20250529170043_init.up.sql
@@ -1,4 +1,6 @@
-CREATE TABLE "users" (
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -9,3 +11,31 @@ CREATE TABLE "users" (
 );
 
 CREATE INDEX users_email_idx ON users (email);
+
+CREATE TABLE roles (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE permissions (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE user_roles (
+    user_id BIGSERIAL NOT NULL,
+    role_id UUID NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+);
+
+CREATE INDEX user_roles_user_id_idx ON user_roles (user_id);
+
+CREATE TABLE role_permissions (
+    role_id UUID NOT NULL,
+    permission_id UUID NOT NULL,
+    PRIMARY KEY (role_id, permission_id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (permission_id) REFERENCES permissions(id) ON DELETE CASCADE
+);
