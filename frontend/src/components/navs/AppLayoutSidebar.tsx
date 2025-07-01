6079180cd616a73fae63cf409d75f7a927d8b861
jerlendds <jerlendds@proton.me>
2025-07-01 10:52:53 -0600

    refactor sidebar styles

--- frontend/src/components/navs/AppLayoutSidebar.tsx
+++ frontend/src/components/navs/AppLayoutSidebar.tsx
@@ -45,10 +45,10 @@ export default function AppLayoutSidebar({ showSidebar, toggleSidebar, logout }:
               <NavLink
                 key={to}
                 to={to}
-                className={`sidebar-link ${showSidebar ? 'mx-2.5' : 'ml-0.5 mr-px'}`}
+                className={({ isActive }) => `flex items-center duration-150 ease-in-out my-1 py-2.5 text-base  rounded-md border   z-50 transition-all pl-3 font-semibold  from-10% pr-1.5 font-display ${isActive ? '  hover:translate-x-px shadow translate-x-px ring-1 ring-primary-400/80 border-primary-400/80 ring-inset bg-gradient-to-tl text-slate-400 from-cod-800/10 to-cod-800/20 ' : 'hover:border-slate-700/10 hover:ring-transparent hover:text-slate-400/60 hover:from-mirage-600/20 hover:to-mirage-600/10  to-mirage-900/10 from-mirage-900/5 border-slate-800/10 shadow hover:translate-x-px text-slate-600/50  -translate-x-px  ring-slate-950/10 ring-2 ring-inset bg-gradient-to-br'}  ${showSidebar ? 'mx-2.5' : 'ml-0 mr-px'}`}
               >
                 <Icon className={
-                  `transition-all mr-2 shrink-0 h-6 w-6 duration-100 ${location.pathname.includes(to) && 'text-slate-400/30'} ${showSidebar ? 'translate-x-0' : 'translate-x-[12.75rem]'}`} />
+                  `transition-all mr-2 shrink-0 h-6 w-6 duration-100 text-slate-600/50 ${location.pathname.includes(to) && '!text-primary '} ${showSidebar ? 'translate-x-0' : 'translate-x-[12.75rem]'}`} />
                 {name}
               </NavLink>
             ))}
@@ -56,16 +56,16 @@ export default function AppLayoutSidebar({ showSidebar, toggleSidebar, logout }:
               <NavLink
                 to="/settings"
                 replace
-                className={`sidebar-link ${showSidebar ? "mx-2.5" : 'ml-0.5 mr-px'} ${location.pathname.includes("settings") && "active mx-2"}`}
+                className={({ isActive }) => `flex items-center duration-300 ease-in-out my-1 py-2.5 text-base  rounded-md border   z-50 transition-all pl-3 font-semibold  from-10% pr-1.5 font-display ${isActive ? '  hover:translate-x-px shadow translate-x-px ring-1 ring-primary-400/80 border-primary-400/80 ring-inset bg-gradient-to-tl text-slate-400 from-cod-800/10 to-cod-800/20 ' : 'hover:border-slate-700/10 hover:ring-transparent hover:text-slate-400/60 hover:from-mirage-600/20 hover:to-mirage-600/10  to-mirage-900/10 from-mirage-900/5 border-slate-800/10 shadow hover:translate-x-px text-slate-600/50  -translate-x-px  ring-slate-950/10 ring-2 ring-inset bg-gradient-to-br'}  ${showSidebar ? 'mx-2.5' : 'ml-0 mr-px'}`}
               >
                 <CogIcon
-                  class={`mr-2 shrink-0 h-6 w-6 duration-100 transition-all ${location.pathname.includes("settings") && '!mr-2 hover:-ml-0.5'} ${showSidebar ? 'translate-x-0' : 'translate-x-[12.75rem]'}`}
+                  class={`mr-2 shrink-0 h-6 w-6 duration-100 transition-all text-slate-600/50 ${location.pathname.includes("settings") && '!mr-2 hover:-ml-0.5 !text-primary'} ${showSidebar ? 'translate-x-0' : 'translate-x-[12.75rem]'}`}
                 />
                 <span>Settings</span> <span class="half-grayscale ml-auto mr-2.5 right-0 opacity-30">🚧</span>
               </NavLink>
               <a
                 href="https://github.com/osintbuddy"
-                class={`sidebar-link ${showSidebar ? 'mx-2.5' : 'ml-0.5 mr-px'}`}
+                class={`flex items-center duration-300 ease-in-out my-1 py-2.5 text-base to-mirage-900/10 from-mirage-900/5 font-medium rounded-md border border-slate-800/10 hover:border-slate-700/10 hover:ring-transparent text-slate-600/50 hover:text-slate-400/60 shadow hover:translate-x-px z-50 transition-all focus:text-slate-400/70 -translate-x-px  ring-slate-950/10 ring-2 ring-inset pl-3  bg-gradient-to-br from-10% pr-1.5 hover:from-mirage-600/20 hover:to-mirage-600/10 font-display ${showSidebar ? 'mx-2.5' : 'ml-0.5 mr-px'}`}
               >
                 <Icon
                   icon="brand-github"
@@ -74,7 +74,7 @@ export default function AppLayoutSidebar({ showSidebar, toggleSidebar, logout }:
                 Github
               </a>
               <a
-                class={`sidebar-link !mb-2 ${showSidebar ? "mx-2.5" : 'ml-0.5 mr-px'}`}
+                class={`flex items-center duration-150 ease-in-out my-1 py-2.5 text-base to-mirage-900/10 from-mirage-900/5 font-medium rounded-md border border-slate-800/10 hover:border-slate-700/10 hover:ring-transparent text-slate-600/50 hover:text-slate-400/60 shadow hover:translate-x-px z-50 transition-all focus:text-slate-400/70 -translate-x-px  ring-slate-950/10 ring-2 ring-inset pl-3  bg-gradient-to-br from-10% pr-1.5 hover:from-mirage-600/20 hover:to-mirage-600/10 font-display !mb-2 ${showSidebar ? "mx-2.5" : 'ml-0.5 mr-px'}`}
                 onClick={() => {
                   logout('')
                   navigate('/', { replace: true })
